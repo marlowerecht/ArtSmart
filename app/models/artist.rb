a40093e4751f87a571f93ae9bca3d8ee20e8496e
Marlowe Recht <recht.marlowe@gmail.com>
2023-03-15 13:10:52 -0400

    added model validations

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,5 @@
 class Artist < ApplicationRecord
     has_many :paintings
+
+    validates :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ApplicationRecord
     has_many :comments
     has_many :paintings, through: :comments
+
+    # tests if passowrd has at least one number, one lowercase letter, one uppercase letter, and is at least 8 characters long
+    PASSWORD_REGEX = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,}$/
+
+    validates :name, presence: true
+    validates :username, presence: true
+    validates :username, uniqueness: true
+    validates :email, presence: true
+    validates :email, uniqueness: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :password, presence: true
+    validates :password, format: { with: PASSWORD_REGEX }
+    validates :password, length: { maximum: 30 }
 end
